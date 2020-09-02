0f8f5c706e56f29400ec021ddc49e26e6ead3b53
opariucbianca <61669184+opariucbianca@users.noreply.github.com>
2020-09-02 21:42:20 +0300

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,42 +1,29 @@
-package com.pluralsight.getorganized;
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Solution {
 
-    // Complete the countingValleys function below.
-    static int countingValleys(int n, String s) {
-        int count = 0;
-        int altitude = 0;
+    static int MaximumToys(int[] prices,int k)
+    {
+        Arrays.sort(prices);
+        for(int i=0;i<prices.length;i++)
+        {
+            k-=prices[i];
+            if(k<=0) return i;
+        }
+        return prices.length;
+    }
+    public static void main(String[] args) {
+        int n,k;
+      Scanner scan=new Scanner(System.in);
+      n=scan.nextInt();
+      k=scan.nextInt();
+        int[] prices=new int[n];
         for (int i = 0; i < n; i++) {
-            char step = s.charAt(i);
-
-            if (altitude == 0 && step == 'D') {
-                count++;
-            }
-            if (step == 'U') {
-                altitude++;
-            } else if (step == 'D') {
-                altitude--;
-            }
-
+            prices[i]=scan.nextInt();
         }
-        return count;
+       int result=MaximumToys(prices,k);
+        System.out.println(result);
     }
-
-        public static void main(String[] args)  {
-            Scanner scan = new Scanner(System.in);
-            int n = scan.nextInt(); scan.nextLine();
-
-            String s = scan.nextLine();
-
-            int result = countingValleys(n, s);
-            System.out.println(result);
-
-        }
-    }
+}
