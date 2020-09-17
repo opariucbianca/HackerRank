d9e73c194eebeb802a4c05c9ab500611acb742ae
opariucbianca <61669184+opariucbianca@users.noreply.github.com>
2020-09-17 17:47:42 +0100

    Lucky Numbers Solution-Greedy

--- main.cpp
+++ main.cpp
@@ -1,17 +1,34 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int arr[100000],n,k,unfairness=100000;
 
+
+int criteriu(int a,int b)
+{
+    if(a<b) return 1;
+    else
+       return 0;
+}
+
+long int n,i,ultimul,k,s=0,x=0,S[1000];
 int main()
 {
+    int a,b;
     cin>>n>>k;
-    for(int i=1;i<=n;i++)
-        cin>>arr[i];
-    sort(arr+1,arr+1+n);
-    for(int i=1;i<=n-k+1;i++)
-        if(arr[i+k-1]-arr[i]<unfairness)
-        unfairness=arr[i+k-1]-arr[i];
-    cout<<unfairness;
+    for(i=1;i<=n;i++)
+    {
+        cin>>a>>b;
+        s+=a;
+        if(b==1)
+            S[++x]=a;
+    }
+    sort(S+1,S+x+1,criteriu);
+    x-=k;
+   for(i=1;i<=x;i++)
+   {
+      s-=2*S[i];
+   }
+   cout<<s<<endl;
+
     return 0;
 }
